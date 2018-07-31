4fd6932d92f30e1949af37480e5ed9fb588efa14
github username <github email address>
2018-07-31 10:03:01 -0400

    patients migration created

--- db/migrate/20180731135240_create_patients_table.rb
+++ db/migrate/20180731135240_create_patients_table.rb
@@ -1,4 +1,9 @@
 class CreatePatientsTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age
+      t.string :sex
+    end
   end
 end
