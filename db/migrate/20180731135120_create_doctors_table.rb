590cbad67a3183c173ef5d202ff45f075019d1e9
github username <github email address>
2018-07-31 10:01:44 -0400

    doctors table created

--- db/migrate/20180731135120_create_doctors_table.rb
+++ db/migrate/20180731135120_create_doctors_table.rb
@@ -1,4 +1,9 @@
 class CreateDoctorsTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :specialization
+      t.decimal :cost
+    end
   end
 end
--- db/migrate/20180731135439_create_bill_of_past_appointments_table.rb
+++
@@ -1,4 +0,0 @@
-class CreateBillOfPastAppointmentsTable < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
