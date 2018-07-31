a871d8622651c9a85df8f03a75df03e1e3b8069f
github username <github email address>
2018-07-31 10:05:31 -0400

    appointments migration created

--- db/migrate/20180731135253_create_appointments_table.rb
+++ db/migrate/20180731135253_create_appointments_table.rb
@@ -1,4 +1,12 @@
 class CreateAppointmentsTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :appointments do |t|
+      t.datetime
+      t.string :condition
+      t.decimal :price
+      t.boolean :paid?
+      t.integer :doctor_id
+      t.integer :patient_id
+    end
   end
 end
