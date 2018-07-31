425531e94d07a855f08e91e63cb5d2ca9f80c127
github username <github email address>
2018-07-31 17:47:30 -0400

    added date parsing

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -10,7 +10,7 @@ class Doctor < ActiveRecord::Base
   end
 
   def schedule_conflicts(date)
-    self.schedule.include?(date)
+    self.schedule.include?(Date.new(date))
   end
   # date = Date.new(2018,8,5)
   # module DPmethods
