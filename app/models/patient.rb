98b38f7c32ac397cd8682acb6a25094871776d63
TylerNodell <Tyler.l.nodell@gmail.com>
2018-08-01 10:18:06 -0400

    chosen specialization

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,14 +4,48 @@ class Patient < ActiveRecord::Base
   has_many :doctors, through: :appointments
   has_many :billings, through: :appointments
 
-  def choose_specialization(specialization)
-    Doctor.all.select do |doctor|
-      doctor.specialization == specialization
+  def speclist
+    uniqspecs = Doctor.all.map do |doctor|
+      doctor.specialization
+    end.uniq
+    uniqspecs.map do |spec|
+      puts "#{uniqspecs.index(spec) + 1}. #{spec}"
     end
+    select_spec(uniqspecs)
   end
+
+  def select_spec(uniqspecs)
+    input = gets.strip.to_i
+
+    chosen_spec = uniqspecs[input - 1]
+
+    docs = Doctor.all.select do |doctor|
+      doctor.specialization == chosen_spec
+    end
+    select_doctor(docs)
+  end
+    
+  def select_doctor(docs)
+    docs.each do |doc|
+      puts "#{docs.index(doc) + 1}. #{doc.name}"
+    end
+
+    input = gets.strip.to_i
+
+    chosen_doctor = docs[input - 1]
+
+    doc_inst = docs.find do |doc|
+      doc == chosen_doctor
+    end
+    puts doc_inst.name
+  end
+
+  
 
   def make_appointment(condition, doctor_id, date)
     Appointment.create(condition: condition, doctor_id: doctor_id, patient_id: self.id, date_and_time: date)
   end
 
 end
+
+
